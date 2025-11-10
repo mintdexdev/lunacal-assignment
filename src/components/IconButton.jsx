2a5cea2c315ccd194033a05c89f659bfa237361d
mintdexdev <dexdevpersona@gmail.com>
2025-11-10 23:38:35 +0530

    active icon color fix

--- src/components/IconButton.jsx
+++ src/components/IconButton.jsx
@@ -4,9 +4,9 @@ export default function IconButton(
     , children, ...props }
 ) {
   return (
-    <button className='icon-button-shadow aspect-square h-[45px] grid place-items-center rounded-full bg-linear-to-br from-[#303439] to-[#161718] duration-300 ease-out hover:from-[#1E252D] active:from-[#95BCE9]'
+    <button className='group icon-button-shadow aspect-square h-[45px] grid place-items-center rounded-full bg-linear-to-br from-[#303439] to-[#161718] duration-300 ease-out hover:from-[#1E252D] active:from-[#95BCE9]'
       type="button" {...props}>
-      <img src={src} alt={alt} />
+      <img className="group-active:brightness-200" src={src} alt={alt} />
       {children}
     </button>
   )
